b6c35a9457c3c57d3c036ae818b871ecc3ef9eb5
+ <+>
2021-01-22 10:13:24 -0500

    commit

--- solution.rb
+++ solution.rb
@@ -1,126 +1,78 @@
 # Code your solution here! 
 
- 
+
 def blue_aliens
-   SELECT name, color FROM aliens WHERE color = 'blue';
+   # return the names and colors of all the aliens that are 'blue'
 end
 
 def old_aliens
-   SELECT name, age FROM aliens WHERE age > 100;
+   # return the names and ages of all aliens over the age of 100
 end
 
 def dangerous_aliens
-   SELECT name FROM aliens WHERE dangerous = true;
+   # return the names of the dangerous aliens
 end
 
 
 def fastest_spaceship
-   SELECT * FROM spaceships ORDER BY speed DESC LIMIT 1;
+ # return all of the info about the fastest spaceship
 end
 
 def aliens_aboard_fastest_spaceship
-   SELECT aliens.name FROM aliens
-   JOIN spaceships
-   ON aliens.spaceship_id = spaceships.id
-   WHERE spaceships.speed = (SELECT MAX(spaceships.speed) FROM spaceships);
+  #return a list of the aliens aboard the fastest spaceship
 end
 
 
 def aliens_and_spaceships
-   SELECT aliens.name, spaceships.name AS spaceship FROM aliens
-   JOIN spaceships
-   ON aliens.spaceship_id = spaceships.id
+  # Return a list of all aliens and the spaceship they belong to
+
 end
 
 
 def aliens_and_planets
-   SELECT aliens.name, planets.name AS planet FROM aliens
-   JOIN planets
-   ON aliens.spaceship_id = planets.id  
+ # Get a list of all aliens and the planets they belong to 
 end
 
 
 def aliens_aboard_beebop
-   SELECT aliens.name FROM aliens
-   JOIN spaceships
-   ON aliens.spaceship_id = spaceships.id
-   WHERE spaceships.name = 'Beebop'
+   # Get a list of all aliens aboard a the spaceship named 'Beebop'  
 end
 
 
 def aliens_from_gliese
-   SELECT aliens.name, planets.name AS planet FROM aliens
-   JOIN planets
-   ON aliens.spaceship_id = planets.id  
-   WHERE planets.name = "Gliese"
+   # Get a list of all aliens from the planet named 'Gliese'
 end
 
 def spaceship_count_for_each_planet
-   SELECT planets.name, COUNT(spaceships.id) AS spaceships FROM planets
-   JOIN spaceships
-   ON spaceships.planet_id = planets.id
-   GROUP BY planets.name
+  # Return each planet's name and how many spaceships are from each planet
 end
 
 def alien_count_for_spaceships
-   SELECT spaceships.name, COUNT(aliens.id) AS num_of_aliens FROM spaceships
-   JOIN aliens
-   ON aliens.spaceship_id = spaceships.id
-   GROUP BY spaceships.name  
+    # Return each spaceships's name and how many aliens are aboard each spaceship    
 end
 
 def alien_count_for_planets
-   SELECT planets.name, COUNT(aliens.id) AS num_of_aliens FROM planets
-   JOIN spaceships ON spaceships.planet_id = planets.id
-   JOIN aliens ON aliens.spaceship_id = spaceships.id
-   GROUP BY planets.name  
+    # Return each planet's name and how many aliens are from each planet  
 end
 
 def order_planets_by_aliens
-   SELECT planets.name, COUNT(aliens.id) AS num_of_aliens FROM planets
-   JOIN spaceships ON spaceships.planet_id = planets.id
-   JOIN aliens ON aliens.spaceship_id = spaceships.id
-   GROUP BY planets.name  
-   ORDER BY num_of_aliens DESC
+   # Order the planets from most number of aliens to least number of aliens
 end
 
 def spaceshisps_with_blue_aliens
-   SELECT spaceships.name FROM spaceships
-   JOIN aliens ON aliens.spaceship_id = spaceships.id
-   WHERE aliens.color = 'blue'
-   GROUP BY spaceships.name
+   # return the names of spaceships that have blue aliens aboard
 end
 
 def spaceship_and_alien_count
- 
-   SELECT planets.name, COUNT(spaceships.id) AS spaceships, (SELECT COUNT(spaceships.planet_id) AS aliens_per_ship 
-   FROM planets
-     JOIN spaceships ON planets.id = spaceships.planet_id 
-       JOIN aliens ON spaceships.id = aliens.spaceship_id 
-       GROUP BY planets.name) AS aliens_per_planet
-    FROM planets
-      JOIN spaceships
-      ON spaceships.planet_id = planets.id
-      GROUP BY planets.name
-
-      ##Couldn't get the correct number of aliens per planet for this one
-   
+  # return a count of all spaceships from a planet and the total number of aliens from that planet
 end
 
 def order_planets_by_alien_count
-   SELECT planets.name, COUNT(aliens.id) AS num_of_aliens FROM planets
-   JOIN spaceships ON spaceships.planet_id = planets.id
-   JOIN aliens ON aliens.spaceship_id = spaceships.id
-   GROUP BY planets.name  
-   ORDER BY num_of_aliens
+   # order the planets based on how many aliens are from that planet
 end
 
 def order_planets_by_old_alien_count
-   SELECT planets.name, COUNT(aliens.id) AS num_of_aliens FROM planets
-   JOIN spaceships ON spaceships.planet_id = planets.id
-   JOIN aliens ON aliens.spaceship_id = spaceships.id
-   WHERE aliens.age > 100
-   GROUP BY planets.name  
-   ORDER BY num_of_aliens 
+   ## MOST DIFFICULT 
+   # order the planets based on how many aliens over the age of 100 are from that planet
 end
 
